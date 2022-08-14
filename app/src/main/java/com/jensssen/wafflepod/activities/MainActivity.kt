c1cb3a078fd01aafa0a758f3e110d2f79a2d0d9b
jensssen <soeren.erichsen@gmx.de>
2022-08-14 21:17:39 +0200

    Add splash image

--- app/src/main/java/com/jensssen/wafflepod/activities/MainActivity.kt
+++ app/src/main/java/com/jensssen/wafflepod/activities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.jensssen.wafflepod
+package com.jensssen.wafflepod.activities
 
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.jensssen.wafflepod.R
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
---
+++ app/src/main/java/com/jensssen/wafflepod/activities/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.jensssen.wafflepod.activities
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.jensssen.wafflepod.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // TODO: Replace this deprecated approach of opening the main activity after 2500 ms
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                // Launch the Main Activity
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()  // Call this when your activity is done and should be closed
+            },
+            2500
+        )
+    }
+}
